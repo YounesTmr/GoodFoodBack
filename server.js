3b27f2c68677c7591ad4d00e0324b41902f94662
younes <younestemar@gmail.com>
2022-09-08 20:37:06 +0200

    changing port to 3000

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening from port ${port}`);
