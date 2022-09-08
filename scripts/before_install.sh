66c110ae15c7ee6bbb283a717713580359a1d1ec
younes <younestemar@gmail.com>
2022-09-08 23:40:13 +0200

    update application start sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install --lts
+nvm install 16
 node -e "console.log('Running Node.js ' + process.version)"
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
