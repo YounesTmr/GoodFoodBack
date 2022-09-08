339d1fb7df1e3c510b9066a24a94247057f9b6ae
younes <younestemar@gmail.com>
2022-09-08 23:30:40 +0200

    change start application bash

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,10 +7,7 @@ sudo chmod -R 777 /home/ec2-user/express-app
 cd /home/ec2-user/express-app
 
 #add npm and node to path
-# export NVM_DIR="$HOME/.nvm"	
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
+export NVM_DIR="/home/ec2-user/.nvm" and [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 #install node modules
 npm install
 
