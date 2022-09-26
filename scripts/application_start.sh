073e6d3ed1b33c6e93f295e94527c9dfdc944043
younes <younestemar@gmail.com>
2022-09-26 15:55:20 +0200

    force install modules for node

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,7 +12,7 @@ export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules 
-npm install
+npm install --legacy-peer-deps
 
 #start our node app in the background
 node server.js > app.out.log 2> app.err.log < /dev/null &  
