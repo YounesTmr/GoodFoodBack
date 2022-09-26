1412f4475beabded99508dfd0553f4105f8652c7
younes <younestemar@gmail.com>
2022-09-26 15:53:23 +0200

    fix bug to reload server

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/userController");
 const authController = require("../controller/authController");
-
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUser);
 
